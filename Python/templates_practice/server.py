9216602ff6b4053637858a93e4c776b9a7e18ff4
MartyMcAlpine <larscodus@gmail.com>
2017-04-12 21:39:55 -0700

    removed test dir

--- Python/templates_practice/server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-@app.route('/')
-def index():
-	return render_template('index.html', phrase='Force Power Rising', times=5)
-app.run(debug=True, port=8888)
