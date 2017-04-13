9b21be7ac797f547707fd741b9cf3d22a0d3bbd1
MartyMcAlpine <larscodus@gmail.com>
2017-04-12 21:19:40 -0700

    file organization

--- Python/hello_flask/server.py
+++ Python/hello_flask/server.py
@@ -3,7 +3,10 @@ import math, random
 from flask import Flask
 from flask import redirect
 from flask import render_template
+from flask import request
+from flask import session
 
+app.secret_key = 'manysecrets1984'
 
 
 app = Flask(__name__)
