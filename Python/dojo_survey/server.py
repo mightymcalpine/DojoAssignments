41422e0d4d5db1f4d131285ec96a3efe4a539325
MartyMcAlpine <larscodus@gmail.com>
2017-04-15 08:57:47 -0700

    updated dojo survey project

--- Python/dojo_survey/server.py
+++ Python/dojo_survey/server.py
@@ -1,22 +1,43 @@
 from flask import Flask
+from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import request
+from flask import session
 
 app = Flask(__name__)
+app.secret_key='secretsauce81'
 
 @app.route('/')
 def index():
+	if 'flash' in session:
+		flash.clear()		
 	return render_template('index.html')
 
 @app.route('/results', methods=['POST'])
 def results():
-	name = request.form['name']
-	location = request.form['location']
-	language = request.form['language']
-	comments = request.form['comments']
-	print name, location, language, comments
-	return render_template('results.html', name=name, location=location, language=language, comments=comments)
+	# name = request.form['name']
+	# location = request.form['location']
+	# language = request.form['language']
+	# comments = request.form['comments']
+	
+	session['name'] = request.form['name']
+	session['location'] = request.form['location']
+	session['language'] = request.form['language']
+	session['comments'] = request.form['comments']
+	
+	if len(session['name']) == 0 or len(session['comments']) == 0:
+		flash('*** Name and Comment fields required ***')
+		return redirect('/')
+		
+	if len(session['comments']) > 120:
+		flash('*** Comments limited to 120 characters ***')
+		return redirect('/')
+	else:
+		pass
+	
+	
+	return render_template('results.html')
 
 
 app.run(debug=True, port=8686)
